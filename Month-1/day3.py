14147eb5c68cc071fc213c99395b799d7617070d
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-26 23:37:44 +0530

    Day 3

---
+++ Month-1/day3.py
@@ -0,0 +1,38 @@
+# day 3
+# 🚀 DAY 3: Input / Output 
+# 🧠 Concept
+
+# Take input from user 👇
+
+name = input("Enter your name: ")
+print(name)
+
+# ===================================================
+# f-string (IMPORTANT)
+print(f"My name is {name}")
+
+
+# ====================================================
+# 🎯 DAY 3 — 10 Tasks
+# Take name input and print it
+# Take age input and print it
+# Take two inputs (name + city)
+# Print using f-string
+# Ask favorite color and print
+# Take number input:
+
+# ==================================================
+name = input("Enter your good name:")
+print(name)
+
+age = input("Enter your Age")
+print(age)
+fcolor = input("Enter your favourite color")
+city = input("Enter your city")
+print(f"My name is {name} and i am {age} and i live in {city} and my Favourite color is {fcolor}.")
+
+num1 = input("enter 1st num ")
+num2 = input("enter num 2")
+print(num1+num2)
+
+# ========================================================
