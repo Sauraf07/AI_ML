7b4d7cb4a45bcd961fc1a758177916077df2d699
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-26 23:48:04 +0530

    day 4

---
+++ Month-1/day4.py
@@ -0,0 +1,37 @@
+# 🚀 DAY 4: Operators
+# Concept
+# ➤ Arithmetic Operators
+# +  -  *  /  %  **
+# ➤ Comparison Operators
+# ==  !=  >  <  >=  <=
+
+# =======================================
+# a = 5
+# b = 2
+# print(a+b) #7
+# print(a>b) #True
+
+# =======================================
+# 🎯 DAY 4 — 10 Tasks
+# Add two numbers
+# Subtract two numbers
+# Multiply two numbers
+# Divide two numbers
+# Find remainder %
+# Compare two numbers (>)
+# Compare equality (==)
+# Check not equal (!=)
+# Take input and compare
+# Create simple calculator:
+
+# ========================================
+a = int(input("Enter num a "))
+b = int(input("Enter num b ")) #int chages string into numbers 
+print(a+b)
+print(b-a)
+print(a/b)
+print(a%b)
+print(a>b)
+print(a==b)
+print(a!=b)
+# -------
