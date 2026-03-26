8b8b78f0f5a4960618b2b2ce74b2e5deb582f1e4
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-26 23:24:33 +0530

    day 2

---
+++ Month-1/day2.py
@@ -0,0 +1,41 @@
+# Variables = containers to store data
+name = "Sauraf"   # string
+age = 20          # int
+height = 5.7      # float
+is_student = True # bool 
+print("Name: ",name,"Age: ",age,"Height:",height,"is a student",is_student) 
+# =================================================================================
+
+# DAY 2 — 10 Tasks
+# Create variable name and print it
+# Create variable age and print it
+# Store a decimal value (height)
+# Store True/False value
+# Print all variables together
+# Change value of variable and print again
+# Create 5 variables in one program
+# Print sentence using variables
+# ===========================================================================================
+
+name = "saurav"
+print(name)
+
+# ===========================================================================================
+
+age = 20
+print(age)
+
+# ==========================================================================================
+
+height = 5.4
+print(height)
+
+# ============================================================================================
+
+is_student = True
+print(is_student)
+
+# ===========================================================================================
+
+sentence = "Python is a high level programming language and is good fro biggners to learn."
+print(sentence)
