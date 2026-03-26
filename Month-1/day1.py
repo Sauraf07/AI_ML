233101ae5d32c20f530c3fb419a0c8f248cee824
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-26 23:13:18 +0530

    day 1

---
+++ Month-1/day1.py
@@ -0,0 +1,12 @@
+# Introduction
+# Python is a programming language used in:
+# AI/ML 🤖
+# Web development 🌐
+# Automation ⚙️
+
+# We write code → computer executes it.
+
+print("Hello world")
+print("MY Name is Saurav Sharma")
+print("I am 20 Years Old")
+print("I am Learning Python")
