95ad2c30bda04078f2de7176ea7298333aa4dedd
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-27 18:49:45 +0530

    day 5 (practice day)

---
+++ Month-1/day5(practice day).py
@@ -0,0 +1,120 @@
+# task 1
+# ✅ Task 1: Personal Info Printer
+
+# Take input:
+# Name
+# Age
+# City
+
+# name = input("enter your name ")
+# age = int(input("Enter your age"))
+# city = input("Enter city")
+# print(f"My name is {name} , I am {age} yeras old, from {city}")
+
+# ==================================================================
+# task 2
+# Task 2: Simple Addition
+# Take 2 numbers from user and print their sum.
+
+# a = int(input("!st no."))
+# b = int(input("2st no."))
+# print(f"Sum {a+b}")
+
+# ===================================================================
+# task 3
+# Area of Rectangle
+# Take:
+# length
+# width
+# 👉 Formula:
+# area = length * width
+
+# length = int(input("Enter length "))
+# width = int(input("enter width "))
+# print(f"Area {length*width}")
+# ====================================================================
+# task 4
+# Swap Two Numbers
+# Without using third variable:
+# a, b = b, a
+
+# a = 5
+# b = 8
+# a,b=b,a
+# print(a,b)
+# ====================================================================
+# task 5
+# Even or Odd (Logic Intro 🔥)
+# Take a number:
+# num % 2
+# 👉 Print remainder (understand logic)
+# num = 5
+# print(num%2)
+# =====================================================================
+# task 6
+# Temperature Converter
+# Take temperature in Celsius
+# Convert to Fahrenheit:
+# F = (C * 9/5) + 32
+# c = 32
+# f = (c*9/5) +32
+# print(f)
+# ================================================================
+# ✅ Task 7: Simple Calculator (Upgrade 💪)
+# Take:
+# 2 numbers
+# operator (+, -, *, /)
+# 👉 Print result
+
+# a = 5
+# b = 9
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+
+# =====================================================================
+# ✅ Task 8: Compare Two Numbers
+# Take two numbers:
+# Print which is greater
+# Print if both are equal
+
+# a = 5
+# b = 7
+# print(a>b)
+# print(a == b)
+
+# ========================================================================
+# ✅ Task 9: Square & Cube
+# Take a number:
+# square = n*n
+# cube = n*n*n
+
+# a = 5
+# print(f"Square {a*a}")
+# print(f"Cube {a*a*a}")
+
+# ===========================================================================
+# ✅ Task 10: Mini Profile Generator 🔥
+# Take input:
+# name
+# age
+# hobby
+# dream job
+
+# name = input("Enter name ")
+# age = input("Enter age ")
+# hobby = input("Enter hobby ")
+# deram_job = input("Enter dream job ")
+# print(f"Hi, I am {name}, I love {hobby}. I am {age} years old, and I want to be a {deram_job}")
+
+# ========================================================================================
+# 🧠 BONUS CHALLENGE (VERY IMPORTANT 💥)
+# 👉 Create ONE program that does ALL:
+# Takes name + 2 numbers
+# Prints:
+# Sum
+# Difference
+# Product
+# Division
+# Full intro 
