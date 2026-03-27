fe5b1aa39dfadf52032c92b6ccaef7f2cf5bd541
Sauraf Kumar <saurafsps7631@gmail.com>
2026-03-27 22:23:47 +0530

    day 6

---
+++ Month-1/day6.py
@@ -0,0 +1,77 @@
+# 🚀 DAY 6: Strings
+# A string = text data
+# name = "Saurav"
+# print(name)
+# ============================================
+# 🔹 1. String Indexing
+
+# Each character has a position:
+# name = "Python"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# ===========================================
+# 🔹 2. String Slicing 🔥
+# name = "Python"
+# print(name[0:3])
+# print(name[2:5])
+# print(name[:5])
+# print(name[3:])
+# =========================================
+# 🔹 3. Important Functions
+# ✅ len()
+# name = "Python"
+# print(len(name))
+
+# ✅ upper() / lower()
+# text = "hello"
+# print(text.lower())
+# print(text.upper())
+
+# ✅ replace()
+# text = "I love India"
+# print(text.replace("India","Myself"))
+# ===============================================
+# 🎯 DAY 6 — 10 Tasks (Practice)
+
+# ✅ Task 1: Take Name Input
+# Print:
+# full name
+# first letter
+# last letter
+
+# name = input("Enter your name ")
+# print(name)
+# print(name[0])
+# print(name[-1])
+
+# ======================================================
+# Task 2: Length Finder
+# Take a string → print length using len()
+# name = input("enter name ")
+# print(len(name))
+# ====================================================
+# ✅ Task 3: Upper & Lower
+# Take input:
+# print in uppercase
+# print in lowercase
+
+# a = input("Enter name ")
+# print(a.upper())
+# print(a.lower())
+# ======================================================
+# ✅ Task 4: String Slicing
+# Take a word → print:
+# first 3 letters
+# last 3 letters
+# name = input("Enter name ")
+# print(name[:3])
+# print((name[3:]))
+# ===========================================================
+# ✅ Task 5: Reverse String 🔥
+# a = input("name")
+# print(a[::-1])
+# ==========================================================
