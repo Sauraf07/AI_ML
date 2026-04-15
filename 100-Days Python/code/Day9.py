9ea509c989b5cae1b229c5221976fecc9aa5e4d1
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-15 15:52:23 +0530

    Day-9

---
+++ 100-Days Python/code/Day9.py
@@ -0,0 +1,13 @@
+a = "Saurav "
+b = "Kumar"
+print(a+b)
+a = "1"
+b = '2'
+print(int(a)+int(b))
+
+
+string = "15"
+number = 7
+string_number = int(string) #throws an error if the string is not a valid integer
+sum= number + string_number
+print("The Sum of both the numbers is: ", sum)
