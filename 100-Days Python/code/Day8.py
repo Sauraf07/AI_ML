07f233c1f2694aacbb6cf2984aa5237f476b9a68
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-15 15:46:00 +0530

    Day-8

---
+++ 100-Days Python/code/Day8.py
@@ -0,0 +1,16 @@
+a = 6
+b = 3
+# Addition
+print("Addition is ",a+b)
+
+# Sub
+print("Subtraction is ",a-b)
+
+# Multiplication 
+print("Multiplicaton is ",a*b)
+
+# Division
+print("Division is " ,a/b)
+
+# flooor division 
+print("Floor division is ",a//b)
