660d6fb9795d8a6de54afb971e3e81c49ae06f95
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-21 21:26:48 +0530

    Day-14

---
+++ 100-Days Python/code/Day14.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age: "))
+print(f"Yur age is {a}")
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if a>18:
+    print("You can drive")
+else:
+    print("You cannot drive ")
