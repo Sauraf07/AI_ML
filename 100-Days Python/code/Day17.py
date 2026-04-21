47582f75824554dbce02ad808fbb8b5a2400746c
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-21 21:47:19 +0530

    Day-17

---
+++ 100-Days Python/code/Day17.py
@@ -0,0 +1,14 @@
+name = "Saurav"
+for i in name:
+    print(i)
+    if i == "S":
+        print("Everything ")
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    if color == "Green":
+        print("This is my favorite color")
+
+for i in range(1,11):
+    print(i)
