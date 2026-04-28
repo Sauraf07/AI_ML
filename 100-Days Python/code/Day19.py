4c82ea7449fa78a20979c43f84f4b01d4f41debd
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-28 09:23:49 +0530

    Day-19

---
+++ 100-Days Python/code/Day19.py
@@ -0,0 +1,11 @@
+for i in range(1,10,1):
+    print(i,end=" ")
+    if i == 9:
+        break
+print("Loop terminated ")
+
+for i in range(1,23):
+    print("Saurav")
+    if i == 20:
+        continue
+print("Kumar") 
