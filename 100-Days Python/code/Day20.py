68a522a31696336516a9bb5f265f1c755baf92d3
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-30 15:28:53 +0530

    Day-20

---
+++ 100-Days Python/code/Day20.py
@@ -0,0 +1,16 @@
+def Hello(name):
+    print(f"Hello {name}")
+
+# Hello("Saurav")
+
+
+def calucator(a,b):
+    print(f"Adition : {a+b}")
+    print(f"Sub : {a-b}")
+    print(f"Multi : {a*b}")
+    print(f"Division : {a/b}")
+    print(f"Floor division : {a//b}")
+    
+calucator(50,20)
+
+
